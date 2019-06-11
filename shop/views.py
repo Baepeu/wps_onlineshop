ced4fc374a72352e965775750e2327d1ec07f0e1
송 종근 <carrotcarrot@Baepeu.local>
2019-06-11 11:31:30 +0900

    sub category

--- shop/views.py
+++ shop/views.py
@@ -10,16 +10,25 @@ class ProductList(ListView):
     template_name = 'shop/product_list.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        # pk = 1 : Home
         categories = Category.objects.filter(parent_category=Category.objects.get(pk=1)).order_by('name')
         kwargs.update({'categories':categories})
         return super().get_context_data(**kwargs)
 
     def get_queryset(self):
+        # 상위 카테고리를 고르면 하위 카테고리 제품들이 한꺼번에 출력되도록 변경
         queryset = super().get_queryset()
         if 'slug' in self.kwargs:
             category = Category.objects.filter(slug=self.kwargs['slug'])
             if category.exists():
-                queryset = queryset.filter(category=category[0])
+                category |= self.get_category_list(category[0])
+                queryset = queryset.filter(category__in=category)
             else:
                 queryset = queryset.none()
         return queryset
+
+    def get_category_list(self,category):
+        categories = category.sub_categories.all()
+        for category in categories:
+            categories |= self.get_category_list(category)
+        return categories
