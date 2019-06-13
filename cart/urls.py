91323dd724f30cafc39f4bd2d2281a2aaec7f762
송 종근 <carrotcarrot@Baepeu.local>
2019-06-13 10:48:16 +0900

    add remove btn

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('add/<int:product_id>/', add_product, name='add_product'),
+    path('remove/<int:product_id>/', remove_product, name='remove_product'),
     path('detail/', cart_detail, name='cart_detail'),
 ]
